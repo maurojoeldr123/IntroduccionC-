08ddb2d239084db42a33b2168f7a4ebbc0b1e33f
maurojoeldr123 <maurojoeldr123@gmail.com>
2020-03-27 19:47:08 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,8 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hola a Todos mi nombre es Mauro Delgado");
+  }
+  //Esto es un ensayo de como funciona Rep.Lit
+}
